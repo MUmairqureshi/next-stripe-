3e6af46132e89f5d69421be94a1149de31c1c102
Briana Gude <briana@dianthe.studio>
2023-09-23 14:53:58 -0400

    remove seed url

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,6 +6,5 @@ export const client = createClient({
   apiVersion,
   dataset,
   projectId,
-  useCdn,
-  token: "skDv5Dtn88YB0RbhtrWJt3brp87ufBcTmtp9DYgEFBRZLecuJqAlJLK2NtFbhycZEl9m7sxoaS0u3ersRecCUZBnkpdQ6jccySoIYM8HHBdi8h2IfZ2e9RRaAiFVssJCRel7X7j0wy0xQNYKHtJOM4GsogkukcogfGXCqt9lHsj5SxbQfDYM"
+  useCdn
 })
